59132bc92e270ac73bfb4a1a19d7613cb4cee1fa
Martin Long <martin@longhome.co.uk>
2021-08-09 22:09:21 +0100

    Added ability to self-update and store state

--- code.py
+++ code.py
@@ -6,6 +6,30 @@ import socketpool
 import wifi
 import adafruit_dotstar
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
+import displayio
+import terminalio
+import adafruit_requests as requests
+import supervisor
+
+from adafruit_display_text import label
+import adafruit_displayio_sh1107
+
+def setdisplay():
+
+    displayio.release_displays()
+
+    # Use for I2C
+    i2c = board.I2C()
+    display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
+
+    # SH1107 is vertically oriented 64x128
+    WIDTH = 128
+    HEIGHT = 64
+    BORDER = 2
+
+    display = adafruit_displayio_sh1107.SH1107(
+        display_bus, width=WIDTH, height=HEIGHT, rotation=0
+    )
 
 # Add a secrets.py to your filesystem that has a dictionary called secrets with "ssid" and
 # "password" keys with your WiFi credentials. DO NOT share that file or commit it into Git or other
@@ -16,6 +40,8 @@ try:
 except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
+
+setdisplay()
 
 dotstar = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.5, auto_write=True)
 
@@ -37,36 +63,39 @@ photocell_feed = secrets["aio_username"] + "/feeds/photocell"
 onoff_feed = secrets["aio_username"] + "/feeds/onoff"
 bright_feed = secrets["aio_username"] + "/feeds/bright"
 rgb_feed = secrets["aio_username"] + "/feeds/rgb"
+username = secrets["aio_username"] 
 
 onoff_status = secrets["aio_username"] + "/feeds/onoff/state"
 bright_status = secrets["aio_username"] + "/feeds/bright/state"
 rgb_status = secrets["aio_username"] + "/feeds/rgb/state"
 
-led_red = pwmio.PWMOut(board.D9, frequency=5000, duty_cycle=0)
-led_green = pwmio.PWMOut(board.D6, frequency=5000, duty_cycle=0)
-led_blue = pwmio.PWMOut(board.D5, frequency=5000, duty_cycle=0)
+update_status = "feeds/updates"
 
-#led_green = pwmio.PWMOut(board.D9, frequency=5000, duty_cycle=0)
-#led_blue = pwmio.PWMOut(board.D6, frequency=5000, duty_cycle=0)
-#led_red = pwmio.PWMOut(board.D5, frequency=5000, duty_cycle=0) 
+led_green = pwmio.PWMOut(board.D9, frequency=5000, duty_cycle=0)
+led_blue = pwmio.PWMOut(board.D6, frequency=5000, duty_cycle=0)
+led_red = pwmio.PWMOut(board.D5, frequency=5000, duty_cycle=0) 
+
 
 red = 255
 green = 255
 blue = 255
 bright = 255
 state = False
+isconnected = False
 
 ### Code ###
 
 def publishstate():
-    #mqtt_client.publish(onoff_status, '{"state": "ON"}')
-    #mqtt_client.publish(bright_status, '{"brightness": "100"}')
-    #mqtt_client.publish(rgb_status, '{"rgb": [10,10,10]}')
     mqtt_client.publish(bright_status, bright)
     mqtt_client.publish(rgb_status, f'{red},{green},{blue}')
     mqtt_client.publish(onoff_status, 'ON' if state else 'OFF')
     mqtt_client.publish(bright_status, bright)
     mqtt_client.publish(rgb_status, f'{red},{green},{blue}')
+    with open("/state.txt", "w") as fp:
+        fp.write('ON\n' if state else 'OFF\n')
+        fp.write('{0:n}\n'.format(bright))
+        fp.write('{0:n},{0:n},{0:n}'.format(red, green, blue))
+        fp.flush()   
 
 def updatedot():
     #print(f"Setting dot {red} {green} {blue} {bright}")
@@ -84,6 +113,8 @@ def updatedot():
 # Define callback methods which are called when events occur
 # pylint: disable=unused-argument, redefined-outer-name
 def connected(client, userdata, flags, rc):
+    global isconnected
+    isconnected = True
     # This function will be called when the client is connected
     # successfully to the broker.
     print("Connected to Adafruit IO! Listening for topic changes on %s" % onoff_feed)
@@ -91,50 +122,91 @@ def connected(client, userdata, flags, rc):
     client.subscribe(onoff_feed)
     client.subscribe(bright_feed)
     client.subscribe(rgb_feed) 
+    client.subscribe(update_status) 
 
-    publishstate()
+    loadstate()
     updatedot()
+    publishstate()
 
 
 def disconnected(client, userdata, rc):
+    global isconnected
     # This method is called when the client is disconnected 
     print("Disconnected from Adafruit IO!")
+    isconnected = False
 
-
-def message(client, topic, message):
+def onoff(message):
     global red
     global green
     global blue
     global bright
     global state
+    red = 255 if bright == 0 else red
+    green = 255 if bright == 0 else green
+    blue = 255 if bright == 0 else blue
+    bright = 255 if bright == 0 else bright
+    if(message == 'ON'):
+        state = True
+    if(message == 'OFF'):
+        state = False
+
+def brightmsg(message):
+    global bright
+    bright = int(message)
+
+def rgbmsg(message):
+    global red
+    global green
+    global blue
+    vals = message.split(",")
+    red = int(vals[0])
+    green = int(vals[1])
+    blue = int(vals[2])
+
+def loadstate(): 
+    with open("/state.txt", "r") as fp:
+        lines = fp.read().splitlines() 
+        onoff(lines[0])
+        brightmsg(lines[1])
+        rgbmsg(lines[2])
+
+def message(client, topic, message):
     # This method is called when a topic the client is subscribed to
     # has a new message.
     print("New message on topic {0}: {1}".format(topic, message))
 
     if(topic == onoff_feed):
-        if(message == 'ON'):
-            state = True
-            red = 255 if bright == 0 else red
-            green = 255 if bright == 0 else green
-            blue = 255 if bright == 0 else blue
-            bright = 255 if bright == 0 else bright
-            updatedot()
-            publishstate()
-        if(message == 'OFF'):
-            state = False
-            updatedot()
-            publishstate()
+        onoff(message)
+        updatedot()
+        publishstate()
     if(topic == bright_feed):
-        bright = int(message)
+        brightmsg(message)
         updatedot()
         publishstate()
     if(topic == rgb_feed):
-        vals = message.split(",")
-        red = int(vals[0])
-        green = int(vals[1])
-        blue = int(vals[2])
+        rgbmsg(message)
         updatedot()
         publishstate()
+    if(topic == update_status):
+        runupdate(message)
+
+
+def runupdate(message):
+    print(f"Got message: {message}")
+    http_get(message)
+
+
+def http_get(url):
+    file = url[url.index('/', url.index('://') + 3):]
+    session = requests.Session(pool)
+    data = session.get(url)
+    print(f"Updating file {file}")
+    with open(file, "w") as fp:
+        for chunk in data.iter_content(1000):
+            fp.write(chunk)
+        fp.flush()
+    mqtt_client.publish(update_status + '/response', f'Successfully updated {username} file {file}')
+    supervisor.reload()
 
 
 # Create a socket pool
@@ -156,16 +228,20 @@ mqtt_client.on_disconnect = disconnected
 mqtt_client.on_message = message
 
 # Connect the client to the MQTT broker.
-print("Connecting to Adafruit IO...")
-mqtt_client.connect()
 
 while True:
-    # Poll the message queue
-    mqtt_client.loop()
+    if(not isconnected) :
+        try :
+            print("Connecting to Adafruit IO...")
+            mqtt_client.connect()
+        except MQTT.MMQTTException:
+            isconnected = False
 
-    # Send a new message
-    #print("Sending photocell value: %d..." % photocell_val)
-    #mqtt_client.publish(photocell_feed, photocell_val)
-    #print("Sent!")
-    #photocell_val += 1
+    # Poll the message queue
+    try:
+        mqtt_client.loop()
+    except MQTT.MMQTTException:
+        isconnected = False
+
     time.sleep(0.1)
+
